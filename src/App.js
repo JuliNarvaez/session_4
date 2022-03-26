6e84211dd576b487d7dc2ddd6f8d625fe701b17d
jnarvaez <jnarvaez@lean-tech.io>
2022-03-26 13:08:16 -0500

    Session_2 and SASS included

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./scss/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="main">
+        <h3 className="main_subtitle">Mentorship - Session 2:</h3>
+        <h1 className="main_title">Webpack Basics 🖥</h1>
+        <ul className="main_list">
+          <li>Create a project with npm, git and webpack</li>
+          <li>Install and use html-loader</li>
+          <li>
+            Get the main HTML element, append information and change colors
+          </li>
+          <li>Create production bundle</li>
+          <li>Use git and upload the project to github</li>
+          <li>Implement HotReload🌶 to Webpack</li>
+        </ul>
+      </div>
     </div>
   );
 }
