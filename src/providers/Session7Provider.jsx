831345623dd6d17e8854724da9287a8749cd4975
Hector Zurga <hjzurga@lean-tech.io>
2022-04-22 12:00:20 -0400

    Alternative to context

--- src/providers/Session7Provider.jsx
+++ src/providers/Session7Provider.jsx
@@ -1,34 +1,46 @@
-import { createContext, useState } from "react";
-import { isEven } from "../utils/isEven";
-import { isDivisibleByTen } from "../utils/isDivisibleByTen";
+import { createContext, useState } from 'react';
+import { isEven } from '../utils/isEven';
+import { isDivisibleByTen } from '../utils/isDivisibleByTen';
 
 const initialNumber = {
-    numValue: 0,
-    isNumEven: false,
-    isNumDivByTen: false
+  numValue: 0,
+  isNumEven: false,
+  isNumDivByTen: false,
 };
 
 export const Session7Context = createContext(initialNumber);
 
 const Session7Provider = ({ children }) => {
-    const [numValue, setNumValue] = useState(initialNumber.numValue);
-    const [isNumEven, setNumEven] = useState(initialNumber.isNumEven);
-    const [isNumDivByTen, setNumDivByTen] = useState(initialNumber.isNumDivByTen);
+  const [state, setState] = useState(initialNumber);
+  // const [numValue, setNumValue] = useState(initialNumber.numValue);
+  // const [isNumEven, setNumEven] = useState(initialNumber.isNumEven);
+  // const [isNumDivByTen, setNumDivByTen] = useState(initialNumber.isNumDivByTen);
 
-    function updateNumber(newNumb) {
-        const numberValue = Number(newNumb);
+  function updateNumber(newNumb) {
+    const numberValue = Number(newNumb);
+    const isNumberDivisible = isDivisibleByTen(numberValue);
+    const isNumberEven = isEven(numberValue);
 
-        setNumValue(numberValue);
-        setNumDivByTen(isDivisibleByTen(numberValue));
-        setNumEven(isEven(numberValue));
-    }
+    setState((prevState) => ({
+      ...prevState,
+      numValue: numberValue,
+      isNumEven: isNumberEven,
+      isNumDivByTen: isNumberDivisible,
+    }));
 
-    return (
-        <Session7Context.Provider value={{ numValue, updateNumber, isNumDivByTen, isNumEven }}>
-            {children}
-        </Session7Context.Provider>
-    )
-}
+    // setNumValue(numberValue);
+    // setNumDivByTen(isDivisibleByTen(numberValue));
+    // setNumEven(isEven(numberValue));
+  }
+
+  return (
+    <Session7Context.Provider
+      // value={{ numValue, updateNumber, isNumDivByTen, isNumEven }}
+      value={{ ...state, updateNumber }}
+    >
+      {children}
+    </Session7Context.Provider>
+  );
+};
 
 export default Session7Provider;
-
